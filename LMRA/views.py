f1896d0006b21a699be5a694c9f59c1273dd5552
gitGibbo <60328148+gitgibbo@users.noreply.github.com>
2023-05-05 22:55:19 +0100

    changes to views (imported login decorator)

--- LMRA/views.py
+++ LMRA/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect
 from django.views.generic.detail import DetailView
 from .models import LMRA, Hazard
 from .forms import LMRAForm, HazardFormSet
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 
