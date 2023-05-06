bcefbe2860129eeb6c2520ff1aaf3efa94cfa377
gitGibbo <60328148+gitgibbo@users.noreply.github.com>
2023-05-06 23:46:06 +0100

    change to urls, to allow fetching of photos

--- safety_app/urls.py
+++ safety_app/urls.py
@@ -17,6 +17,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from LMRA.views import homepage
+from django.conf.urls.static import static
+from django.conf import settings
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -24,4 +27,4 @@ urlpatterns = [
     path('lmra/', include('LMRA.urls')),
     path('users/', include('users.urls')),
     path('unsafe/', include('unsafe_condition.urls'))
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
